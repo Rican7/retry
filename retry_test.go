d4321d9bb0bf82982d7e2523391f0725fe3ea032
Trevor Suarez <rican7@gmail.com>
2016-06-24 21:30:56 -0400

    New tests for the `retry.shouldAttempt` function

--- retry_test.go
+++ retry_test.go
@@ -46,3 +46,77 @@ func TestRetryRetriesUntilNoErrorReturned(t *testing.T) {
 		)
 	}
 }
+
+func TestShouldAttempt(t *testing.T) {
+	shouldAttempt := shouldAttempt(1)
+
+	if !shouldAttempt {
+		t.Error("expected to return true")
+	}
+}
+
+func TestShouldAttemptWithStrategy(t *testing.T) {
+	const attemptNumberShouldReturnFalse = 7
+
+	strategy := func(attempt uint) bool {
+		return (attemptNumberShouldReturnFalse != attempt)
+	}
+
+	should := shouldAttempt(1, strategy)
+
+	if !should {
+		t.Error("expected to return true")
+	}
+
+	should = shouldAttempt(1 + attemptNumberShouldReturnFalse, strategy)
+
+	if !should {
+		t.Error("expected to return true")
+	}
+
+	should = shouldAttempt(attemptNumberShouldReturnFalse, strategy)
+
+	if should {
+		t.Error("expected to return false")
+	}
+}
+
+func TestShouldAttemptWithMultipleStrategies(t *testing.T) {
+	trueStrategy := func(attempt uint) bool {
+		return true
+	}
+
+	falseStrategy := func(attempt uint) bool {
+		return false
+	}
+
+	should := shouldAttempt(1, trueStrategy)
+
+	if !should {
+		t.Error("expected to return true")
+	}
+
+	should = shouldAttempt(1, falseStrategy)
+
+	if should {
+		t.Error("expected to return false")
+	}
+
+	should = shouldAttempt(1, trueStrategy, trueStrategy, trueStrategy)
+
+	if !should {
+		t.Error("expected to return true")
+	}
+
+	should = shouldAttempt(1, falseStrategy, falseStrategy, falseStrategy)
+
+	if should {
+		t.Error("expected to return false")
+	}
+
+	should = shouldAttempt(1, trueStrategy, trueStrategy, falseStrategy)
+
+	if should {
+		t.Error("expected to return false")
+	}
+}
