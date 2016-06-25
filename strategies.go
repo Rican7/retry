e4dada1c1d317751ff42fbef6aabded37f6988e5
Trevor Suarez <rican7@gmail.com>
2016-06-24 22:04:06 -0400

    3 new time-based strategies! Woot!!!
    Now if only I could figure out how to test them...

--- strategies.go
+++ strategies.go
@@ -1,9 +1,44 @@
 package retry
 
+import (
+	"time"
+)
+
 // AttemptLimiter creates a Strategy that limits the number of attempts that
 // Retry will make
 func AttemptLimiter(attemptLimit uint) Strategy {
 	return func(attempt uint) bool {
 		return (attempt < attemptLimit)
+	}
+}
+
+// Delay creates a Strategy that waits the given duration on the first attempt
+func Delay(duration time.Duration) Strategy {
+	return func(attempt uint) bool {
+		if (0 == attempt) {
+			time.Sleep(duration)
+		}
+
+		return true
+	}
+}
+
+// Wait creates a Strategy that waits the given duration after the first attempt
+func Wait(duration time.Duration) Strategy {
+	return func(attempt uint) bool {
+		if (0 < attempt) {
+			time.Sleep(duration)
+		}
+
+		return true
+	}
+}
+
+// Backoff creates a Strategy that waits an increasing duration
+func Backoff(duration time.Duration) Strategy {
+	return func(attempt uint) bool {
+		time.Sleep(duration * time.Duration(attempt))
+
+		return true
 	}
 }
