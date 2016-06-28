f8fe3bcc9bf2c2f74ad7319e49a70d32f01feb6a
Trevor Suarez <rican7@gmail.com>
2016-06-28 03:32:21 -0400

    New tests for the time-based strategies

--- strategies_test.go
+++ strategies_test.go
@@ -2,6 +2,7 @@ package retry
 
 import (
 	"testing"
+	"time"
 )
 
 func TestLimit(t *testing.T) {
@@ -19,5 +20,39 @@ func TestLimit(t *testing.T) {
 
 	if strategy(3) {
 		t.Error("strategy expected to return false")
+	}
+}
+
+func TestDelay(t *testing.T) {
+	const delayDuration = time.Duration(10 * time.Millisecond)
+
+	strategy := Delay(delayDuration)
+
+	if now := time.Now(); !strategy(0) || delayDuration > time.Since(now) {
+		t.Errorf(
+			"strategy expected to return true in %s",
+			time.Duration(delayDuration),
+		)
+	}
+
+	if now := time.Now(); !strategy(5) || 0 != time.Since(now) {
+		t.Error("strategy expected to return true in 0 time")
+	}
+}
+
+func TestWait(t *testing.T) {
+	const waitDuration = time.Duration(10 * time.Millisecond)
+
+	strategy := Wait(waitDuration)
+
+	if now := time.Now(); !strategy(0) || 0 != time.Since(now) {
+		t.Error("strategy expected to return true in 0 time")
+	}
+
+	if now := time.Now(); !strategy(1) || waitDuration > time.Since(now) {
+		t.Errorf(
+			"strategy expected to return true in %s",
+			time.Duration(waitDuration),
+		)
 	}
 }
