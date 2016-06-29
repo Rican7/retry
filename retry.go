fb1277dafe401725a3d246e86d2d47bb292db35b
Trevor Suarez <rican7@gmail.com>
2016-06-28 20:37:33 -0400

    Improving docs a bit

--- retry.go
+++ retry.go
@@ -10,11 +10,17 @@ type Action func(attempt uint) error
 // Strategy defines a function that Retry calls before every successive attempt
 // to determine whether it should make the next attempt or not. Returning `true`
 // allows for the next attempt to be made. Returning `false` halts the retrying
-// process and returns the last error returned by the `Action`.
+// process and returns the last error returned by the called `Action`.
+//
+// The strategy will be passed an "attempt" number on each successive retry
+// iteration, starting with a `0` value before the first attempt is actually
+// made. This allows for a pre-action delay, etc.
 type Strategy func(attempt uint) bool
 
-// Retry takes an action and performs it until successful, as defined by the
-// provided strategy.
+// Retry takes an action and performs it, repetitively, until successful.
+//
+// Optionally, strategies may be passed that assess whether or not an attempt
+// should be made.
 func Retry(action Action, strategies ...Strategy) error {
 	var err error
 
