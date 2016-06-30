d815bf8192cc0386a2a444041434c3c9e705996b
Trevor Suarez <rican7@gmail.com>
2016-06-30 01:10:25 -0400

    Renaming "initial" to "factor". Still not sure I like it...

--- strategy/backoff.go
+++ strategy/backoff.go
@@ -10,41 +10,41 @@ import (
 
 // BackoffAlgorithm defines a function that calculates a time.Duration based on
 // a given duration and retry attempt number.
-type BackoffAlgorithm func(initial time.Duration, attempt uint) time.Duration
+type BackoffAlgorithm func(factor time.Duration, attempt uint) time.Duration
 
-// Backoff creates a Strategy that waits before each attempt, with an increasing
-// duration.
-func Backoff(initial time.Duration, algorithm BackoffAlgorithm) Strategy {
+// Backoff creates a Strategy that waits before each attempt, with a duration as
+// defined by the given factor duration and BackoffAlgorithm.
+func Backoff(factor time.Duration, algorithm BackoffAlgorithm) Strategy {
 	return func(attempt uint) bool {
 		if 0 < attempt {
-			time.Sleep(algorithm(initial, attempt))
+			time.Sleep(algorithm(factor, attempt))
 		}
 
 		return true
 	}
 }
 
-// Incremental creates a BackoffAlgorithm that increments the initial duration
+// Incremental creates a BackoffAlgorithm that increments the factor duration
 // by the given increment for each attempt.
 func Incremental(increment time.Duration) BackoffAlgorithm {
-	return func(initial time.Duration, attempt uint) time.Duration {
-		return initial + (increment * time.Duration(attempt))
+	return func(factor time.Duration, attempt uint) time.Duration {
+		return factor + (increment * time.Duration(attempt))
 	}
 }
 
-// Linear creates a BackoffAlgorithm that linearly multiplies the initial
+// Linear creates a BackoffAlgorithm that linearly multiplies the factor
 // duration by the attempt number for each attempt.
 func Linear() BackoffAlgorithm {
-	return func(initial time.Duration, attempt uint) time.Duration {
-		return (initial * time.Duration(attempt))
+	return func(factor time.Duration, attempt uint) time.Duration {
+		return (factor * time.Duration(attempt))
 	}
 }
 
-// Exponential creates a BackoffAlgorithm that multiplies the initial duration
+// Exponential creates a BackoffAlgorithm that multiplies the factor duration
 // by an exponentially increasing factor for each attempt, where the factor is
 // calculated as the given base raised to the attempt number.
 func Exponential(base float64) BackoffAlgorithm {
-	return func(initial time.Duration, attempt uint) time.Duration {
-		return (initial * time.Duration(math.Pow(base, float64(attempt))))
+	return func(factor time.Duration, attempt uint) time.Duration {
+		return (factor * time.Duration(math.Pow(base, float64(attempt))))
 	}
 }
