8b87f7b7b233efdcad743788bbcb3331dd547d1a
Trevor Suarez <rican7@gmail.com>
2016-07-01 01:46:35 -0400

    New tests for the new behaviors

--- strategy/backoff_test.go
+++ strategy/backoff_test.go
@@ -22,7 +22,37 @@ func TestBackoff(t *testing.T) {
 	}
 
 	for i := uint(1); i < 10; i++ {
-		expectedResult := backoffDuration - (algorithmDurationBase * time.Duration(i))
+		expectedResult := algorithm(i)
+
+		if now := time.Now(); !strategy(i) || expectedResult > time.Since(now) {
+			t.Errorf(
+				"strategy expected to return true in %s",
+				expectedResult,
+			)
+		}
+	}
+}
+
+func TestBackoffWithJitter(t *testing.T) {
+	const backoffDuration = time.Duration(10 * time.Millisecond)
+	const algorithmDurationBase = time.Millisecond
+
+	algorithm := func(attempt uint) time.Duration {
+		return backoffDuration - (algorithmDurationBase * time.Duration(attempt))
+	}
+
+	transformation := func(duration time.Duration) time.Duration {
+		return duration - time.Duration(10 * time.Millisecond)
+	}
+
+	strategy := BackoffWithJitter(algorithm, transformation)
+
+	if now := time.Now(); !strategy(0) || 0 != time.Since(now) {
+		t.Error("strategy expected to return true in 0 time")
+	}
+
+	for i := uint(1); i < 10; i++ {
+		expectedResult := transformation(algorithm(i))
 
 		if now := time.Now(); !strategy(i) || expectedResult > time.Since(now) {
 			t.Errorf(
@@ -106,6 +136,20 @@ func TestFibonacci(t *testing.T) {
 
 		if result != expected {
 			t.Errorf("algorithm expected to return a %s duration, but received %s instead", expected, result)
+		}
+	}
+}
+
+func TestNone(t *testing.T) {
+	transformation := none()
+
+	for i := uint(0); i < 10; i++ {
+		duration := time.Duration(i) * time.Millisecond
+		result := transformation(duration)
+		expected := duration
+
+		if result != expected {
+			t.Errorf("transformation expected to return a %s duration, but received %s instead", expected, result)
 		}
 	}
 }
