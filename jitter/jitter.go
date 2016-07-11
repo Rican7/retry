6b82728b46a942c0a2adee7329714ffe1f510817
Trevor Suarez <rican7@gmail.com>
2016-07-11 03:23:45 -0400

    3 new jitter aglorithms:
    - `EqualRandom`
    - `Deviation`
    - `NormalDistribution`

--- jitter/jitter.go
+++ jitter/jitter.go
@@ -4,6 +4,7 @@
 package jitter
 
 import (
+	"math"
 	"math/rand"
 	"time"
 )
@@ -12,8 +13,8 @@ import (
 // the given duration.
 type Transformation func(duration time.Duration) time.Duration
 
-// FullRandom creates a Transformation that transforms a duration into a
-// result duration in [0, n), where n is the given duration.
+// FullRandom creates a Transformation that transforms a duration into a result
+// duration in [0, n) randomly, where n is the given duration.
 //
 // The given generator is what is used to determine the random transformation.
 // If a nil generator is passed, a default one will be provided.
@@ -24,6 +25,53 @@ func FullRandom(generator *rand.Rand) Transformation {
 
 	return func(duration time.Duration) time.Duration {
 		return time.Duration(random.Int63n(int64(duration)))
+	}
+}
+
+// EqualRandom creates a Transformation that transforms a duration into a result
+// duration in [n/2, n) randomly, where n is the given duration.
+//
+// The given generator is what is used to determine the random transformation.
+// If a nil generator is passed, a default one will be provided.
+//
+// Inspired by https://www.awsarchitectureblog.com/2015/03/backoff.html
+func EqualRandom(generator *rand.Rand) Transformation {
+	random := fallbackNewRandom(generator)
+
+	return func(duration time.Duration) time.Duration {
+		return (duration / 2) + time.Duration(random.Int63n(int64(duration))/2)
+	}
+}
+
+// Deviation creates a Transformation that transforms a duration into a result
+// duration that deviates from the input randomly by a given factor.
+//
+// The given generator is what is used to determine the random transformation.
+// If a nil generator is passed, a default one will be provided.
+//
+// Inspired by https://developers.google.com/api-client-library/java/google-http-java-client/backoff
+func Deviation(generator *rand.Rand, factor float64) Transformation {
+	random := fallbackNewRandom(generator)
+
+	return func(duration time.Duration) time.Duration {
+		min := int64(math.Floor(float64(duration) * (1 - factor)))
+		max := int64(math.Ceil(float64(duration) * (1 + factor)))
+
+		return time.Duration(random.Int63n(max-min) + min)
+	}
+}
+
+// NormalDistribution creates a Transformation that transforms a duration into a
+// result duration based on a normal distribution of the input and the given
+// standard deviation.
+//
+// The given generator is what is used to determine the random transformation.
+// If a nil generator is passed, a default one will be provided.
+func NormalDistribution(generator *rand.Rand, standardDeviation float64) Transformation {
+	random := fallbackNewRandom(generator)
+
+	return func(duration time.Duration) time.Duration {
+		return time.Duration(random.NormFloat64()*standardDeviation + float64(duration))
 	}
 }
 
