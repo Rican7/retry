e7232b1f78fb37e3afc7fc4e1ac6b09016d62a45
Trevor Suarez <rican7@gmail.com>
2016-07-11 22:52:54 -0400

    Separating the top-level examples into their own file and test sub-package.
    It makes the examples easier to copy (their package scope is more
    accurate), and provides a nice separation between documentation examples
    and actual integration test examples.

--- retry_test.go
+++ retry_test.go
@@ -2,17 +2,7 @@ package retry
 
 import (
 	"errors"
-	"fmt"
-	"log"
-	"math/rand"
-	"net/http"
-	"os"
 	"testing"
-	"time"
-
-	"github.com/Rican7/retry/backoff"
-	"github.com/Rican7/retry/jitter"
-	"github.com/Rican7/retry/strategy"
 )
 
 func TestRetry(t *testing.T) {
@@ -129,72 +119,4 @@ func TestShouldAttemptWithMultipleStrategies(t *testing.T) {
 	if should {
 		t.Error("expected to return false")
 	}
-}
-
-func Example() {
-	Retry(func(attempt uint) error {
-		return nil // Do something that may or may not cause an error
-	})
-}
-
-func Example_fileOpen() {
-	const logFilePath = "/var/log/myapp.log"
-
-	var logFile *os.File
-
-	err := Retry(func(attempt uint) error {
-		var err error
-
-		logFile, err = os.Open(logFilePath)
-
-		return err
-	})
-
-	if nil != err {
-		log.Fatalf("Unable to open file %q with error %q", logFilePath, err)
-	}
-}
-
-func Example_httpGetWithStrategies() {
-	var response *http.Response
-
-	action := func(attempt uint) error {
-		var err error
-
-		response, err = http.Get("https://api.github.com/repos/Rican7/retry")
-
-		if nil == err && nil != response && response.StatusCode > 200 {
-			err = fmt.Errorf("failed to fetch (attempt #%d) with status code: %d", attempt, response.StatusCode)
-		}
-
-		return err
-	}
-
-	err := Retry(
-		action,
-		strategy.Limit(5),
-		strategy.Backoff(backoff.Fibonacci(10*time.Millisecond)),
-	)
-
-	if nil != err {
-		log.Fatalf("Failed to fetch repository with error %q", err)
-	}
-}
-
-func Example_withBackoffJitter() {
-	action := func(attempt uint) error {
-		return errors.New("something happened")
-	}
-
-	seed := time.Now().UnixNano()
-	random := rand.New(rand.NewSource(seed))
-
-	Retry(
-		action,
-		strategy.Limit(5),
-		strategy.BackoffWithJitter(
-			backoff.BinaryExponential(10*time.Millisecond),
-			jitter.Deviation(random, 0.5),
-		),
-	)
 }
