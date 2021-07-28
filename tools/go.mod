d1c51636cf886077557bfa1be8322fd51f108184
Trevor Suarez <rican7@gmail.com>
2021-07-28 02:16:31 -0600

    Adding a tools sub-module
    Inspired by the Go wiki, but also by Uber's approach:
    
     - https://github.com/uber-go/multierr/pull/38
     - https://github.com/uber-go/zap/pull/914

---
+++ tools/go.mod
@@ -0,0 +1,8 @@
+module github.com/Rican7/retry/tools
+
+go 1.11
+
+require (
+	golang.org/x/lint v0.0.0-20210508222113-6edffad5e616
+	golang.org/x/tools v0.1.5
+)
---
+++ tools/tools.go
@@ -0,0 +1,16 @@
+// Package tools provides tools for development.
+//
+// It follows the pattern set-forth in the wiki:
+//  - https://github.com/golang/go/wiki/Modules#how-can-i-track-tool-dependencies-for-a-module
+//  - https://github.com/go-modules-by-example/index/tree/4ea90b07f9/010_tools
+//
+// Copyright © 2021 Trevor N. Suarez (Rican7)
+//
+// +build tools
+package tools
+
+import (
+	// Tools for development
+	_ "golang.org/x/lint/golint"
+	_ "golang.org/x/tools/cmd/goimports"
+)
